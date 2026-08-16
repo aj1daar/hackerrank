3fc8d4f83dcd79a8f42b9f97f615b95925abb787
Atai Nogoibaev <77335189+aj1daar@users.noreply.github.com>
2026-08-16 15:52:21 +0200

    ✅ Java Loops I | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-16

--- hackerrank/medium/java-loops-i/solution.java
+++ hackerrank/medium/java-loops-i/solution.java
@@ -1,19 +1,22 @@
-import java.util.Scanner;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
 
 public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
-            Scanner sc=new Scanner(System.in);
-            System.out.println("================================");
-            for(int i=0;i<3;i++){
-                String s1=sc.next();
-                int x=sc.nextInt();
-                System.out.printf("%-15s%03d%n", s1, x);
-            }
-            System.out.println("================================");
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        for (int i =1; i < 11; i++){
+            System.out.printf( "%s x %s = %s%n", N, i, (i * N));
+        }
 
+        bufferedReader.close();
     }
 }
-
-
-
